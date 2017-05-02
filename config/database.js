a154bbf93e76aa167ca18adbf711d4740be2fe23
neetu <neetuy1455@gmail.com>
2017-05-02 14:24:03 +0530

    initial commit

---
+++ config/database.js
@@ -0,0 +1,11 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'root'
+    },
+	'database': 'database_practice',
+    'users_table': 'users'
+};
+
